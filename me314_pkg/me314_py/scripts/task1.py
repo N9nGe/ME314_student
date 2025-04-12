52be0d4625919a67d73f4042bd47885b50bf8781
ngao4 <ngao4@illinois.edu>
2025-04-11 22:52:51 -0700

    finish find and locate the object but face a weird bug

--- me314_pkg/me314_py/scripts/task1.py
+++ me314_pkg/me314_py/scripts/task1.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
+import cv2
+import numpy as np
+import time
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PointStamped
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Image
-import cv2
-import numpy as np
+from tf2_ros import Buffer, TransformListener
+import tf2_geometry_msgs
+from builtin_interfaces.msg import Time
 
 # Import the command queue message types from the reference code
 from me314_msgs.msg import CommandQueue, CommandWrapper
@@ -31,8 +35,18 @@ class PickPlace(Node):
         self.rgb_subscription = self.create_subscription(Image,'/color/image_raw', self.color_image_callback, 10)
         self.depth_subscription = self.create_subscription(Image,'/aligned_depth_to_color/image_raw', self.depth_image_callback, 10)
 
-        self.rgb_image = None
-        self.depth_image = None
+        self.last_red_pixel = None
+        self.fx = 640.5098266601562
+        self.fy = 640.5098266601562
+        self.cx = 640.0
+        self.cy = 360.0
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        self.target_pose = None
+        self.object_locked = False
+        self.origin_pose = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]  # Default origin pose
 
     def arm_pose_callback(self, msg: Pose):
         self.current_arm_pose = msg
@@ -41,20 +55,60 @@ class PickPlace(Node):
         self.current_gripper_position = msg.data
 
     def color_image_callback(self, msg: Image):
+        if self.object_locked:
+            return
         # encoding = rgb8
+        
         raw_image = np.frombuffer(msg.data, dtype=np.uint8).reshape((msg.height, msg.width, 3))
         rgb_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
         # Save the image for testing
         # cv2.imwrite('image_rgb.png', rgb_image)
-        self.rgb_image = rgb_image
+
+        # get the center of the red box
+        center = self.red_box_segmentation(raw_image)
+        if center is not None:
+            cx, cy = center
+            self.get_logger().info(f"Red object center: ({cx}, {cy})")
+
+            # Test code
+            # rgb_image_marked = rgb_image.copy()
+            # cv2.circle(rgb_image_marked, (cx, cy), 8, (0, 255, 0), thickness=2)  # 绿色圆圈
+            # cv2.imwrite('rgb_image_marked.png', rgb_image_marked)
+
+            # store the current coordinate
+            self.last_red_pixel = (cx, cy)
+        else:
+            self.get_logger().info("Red object not found.")
 
 
     def depth_image_callback(self, msg: Image):
-        depth_image = np.frombuffer(msg.data, dtype=np.uint16).reshape((msg.height, msg.width))
-        depth_image = cv2.normalize(depth_image, None, 0, 255, cv2.NORM_MINMAX)
+        if self.object_locked or self.last_red_pixel is None:
+            return
+        depth_image_raw = np.frombuffer(msg.data, dtype=np.uint16).reshape((msg.height, msg.width))
+        # for display 
+        depth_image = cv2.normalize(depth_image_raw, None, 0, 255, cv2.NORM_MINMAX)
         depth_image = depth_image.astype(np.uint8)
-        self.depth_image = depth_image
-        # cv2.imwrite('depth_display.png', depth_display)
+        # cv2.imwrite('depth_display.png', depth_image)
+        if hasattr(self, 'last_red_pixel'):
+            u, v = self.last_red_pixel
+            z_mm = depth_image_raw[v, u] 
+
+            if z_mm == 0:
+                self.get_logger().warn("Invalid depth at red object pixel.")
+                return
+
+            x, y, z = self.pixel_to_camera_point(u, v, z_mm)
+            point_cam = [x, y, z]
+            point_world = self.transform_camera_to_world_tf(point_cam)
+
+            if point_world is not None:
+                self.target_pose = [point_world[0], point_world[1], point_world[2], 1.0, 0.0, 0.0, 0.0]  
+                self.object_locked = True 
+                self.publish_pose(self.target_pose)
+                self.publish_gripper_position(1.0)  # Close the gripper
+                time.sleep(2.0)
+                self.publish_pose(self.origin_pose)
+                
 
     def publish_pose(self, pose_array: list):
         """
@@ -109,28 +163,67 @@ class PickPlace(Node):
         
         self.get_logger().info(f"Published gripper command to queue: {gripper_pos:.2f}")
 
+    def red_box_segmentation(self, rgb_image):
+        # rgb mask
+        lower_red = np.array([150, 0, 0])
+        upper_red = np.array([255, 100, 100])
 
+        red_mask = cv2.inRange(rgb_image, lower_red, upper_red)
+        cv2.imwrite("red_mask_rgb.png", red_mask)
+        # find the largest contour
+        contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            return None  # no red box found
+        largest_contour = max(contours, key=cv2.contourArea)
+
+        # get the center of mass
+        M = cv2.moments(largest_contour)
+        if M["m00"] == 0:
+            return None
+
+        cx = int(M["m10"] / M["m00"])
+        cy = int(M["m01"] / M["m00"])
+        return (cx, cy)
+    
+    def pixel_to_camera_point(self, u, v, z_mm):
+        z_m = z_mm / 1000.0  # mm -> m
+        x = (u - self.cx) * z_m / self.fx
+        y = (v - self.cy) * z_m / self.fy
+        return (x, y, z_m)
+    
+    def transform_camera_to_world_tf(self, point_cam, frame='camera_depth_optical_frame'):
+        point_msg = PointStamped()
+        point_msg.header.frame_id = frame
+        # point_msg.header.stamp = self.get_clock().now().to_msg()
+        point_msg.header.stamp = Time()
+        point_msg.point.x = point_cam[0]
+        point_msg.point.y = point_cam[1]
+        point_msg.point.z = point_cam[2]
+
+        try:
+            point_world = self.tf_buffer.transform(point_msg, 'world', timeout=rclpy.duration.Duration(seconds=0.5))
+            return (point_world.point.x, point_world.point.y, point_world.point.z)
+        except Exception as e:
+            self.get_logger().warn(f"TF transform failed: {str(e)}")
+            return None
+    
 def main(args=None):
     rclpy.init(args=args)
     node = PickPlace()
-    
-    #TODO: test the color image first
-    node.get_logger().info("Opening gripper...")
-    node.publish_gripper_position(0.0)
-    p0 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
-    # TODO: Test the start pose of the robot arm
-    # node.get_logger().info(f"Publishing Pose {i+1}...")
-    node.publish_pose(p0)
 
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    # # Define poses using the array format [x, y, z, qx, qy, qz, qw]
-    # p0 = [0.3408, 0.0021, 0.3029, 1.0, 0.0, 0.0, 0.0]
-    # p1 = [p0[0], p0[1], 0.1, 1.0, 0.0, 0.0, 0.0]
+    # try:
+    #     rclpy.spin(node)
+    # except KeyboardInterrupt:
+    #     pass
+
+    # Define poses using the array format [x, y, z, qx, qy, qz, qw]
+    p0 = [0.35442898432566816, 0.08758732426157431, 0.007964988540690887, 1.0, 0.0, 0.0, 0.0]
+    p1 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
 
     # poses = [p0, p1]
+    node.publish_pose(p0)
+    node.publish_gripper_position(1.0)
+    node.publish_pose(p1)
 
     # # Let's first open the gripper (0.0 to 1.0, where 0.0 is fully open and 1.0 is fully closed)
     # node.get_logger().info("Opening gripper...")
